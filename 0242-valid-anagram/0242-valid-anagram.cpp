22f29f4aed5c15a877e7becb8b69784f3694015b
SATYAM <sk6643614@gmail.com>
2026-07-15 16:24:59 +0530

    Time: 0 ms (100%), Space: 9.5 MB (92.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,8 +3,16 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return false;
 
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-       return s==t;
+        int freq[26] = {0};
+
+        for(int i = 0;i < s.size();i++){
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+        for(int i = 0;i < 26;i++){
+            if(freq[i] != 0)
+            return false;
+        }
+        return true;
     }
 };
