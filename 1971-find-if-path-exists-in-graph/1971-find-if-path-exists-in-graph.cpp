4cd389ccea4e2b536031ed09ba16ba7213aa27d1
SATYAM <sk6643614@gmail.com>
2026-09-23 17:12:19 +0530

    Time: 262 ms (74.99%), Space: 298.7 MB (61.08%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        vector<vector<int>> adj(n);
+bool dfs(int node, int destination,
+             vector<vector<int>>& adj,
+             vector<bool>& visited) {
 
-        for(auto &edge : edges){
+        if (node == destination)
+            return true;
+
+        visited[node] = true;
+
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                if (dfs(neighbor, destination, adj, visited))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+       vector<vector<int>> adj(n);
+
+        for (auto &edge : edges) {
             int u = edge[0];
             int v = edge[1];
 
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
+
         vector<bool> visited(n, false);
-        queue<int> q;
 
-        q.push(source);
-        visited[source] = true;
-
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-
-            if (node == destination)
-                return true;
-
-            for (int neighbor : adj[node]) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    q.push(neighbor);
-                }
-            }
-        }
-
-        return false;
+        return dfs(source, destination, adj, visited);
     }
 };
